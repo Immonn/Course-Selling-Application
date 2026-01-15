3835234a2b09e62623d6b73775c6adcd64b6714e
Immonn <tgautam859@gmail.com>
2026-01-15 23:56:35 +0530

    Initialize vite for frontend and created DB Schema

---
+++ Backend/src/db.ts
@@ -0,0 +1,30 @@
+import {Schema,model,ObjectId,Types} from "mongoose";
+
+
+const userSchema=new Schema({
+    username:{type:String,required:true,unique:true},
+    password:String,
+    role:{
+        type:String,
+        enum:["admin","user"],
+        default:"user"
+    }
+})
+
+const CourseSchema=new Schema({
+    title:{type:String,unique:true,required:true},
+    description:String,
+    imageUrl:String,
+    price:Number,
+    createrId:Types.ObjectId
+})
+
+
+const purchaseSchema=new Schema({
+    userId:Types.ObjectId,
+    courseID:Types.ObjectId
+})
+
+export const userModel=model("User",userSchema)
+export const courseModel=model("Courses",CourseSchema)
+export const purchaseModel=model("Purchases",purchaseSchema)
